da17e1f3cf2c42117a26749944f923cba5152857
Julien Mourer <getkey@getkey.eu>
2020-05-26 14:39:54 +0200

    Validate timings and level name

--- src/validator.test.ts
+++ src/validator.test.ts
@@ -1,6 +1,66 @@
 import { validator } from './validator';
-import myLevel from './my_level.json';
+import myLevel from './levelExamples/my_level.json';
 
-test('basic valid level', () => {
+test('is an object', () => {
+	expect(() => validator([])).toThrow();
+	expect(() => validator(342)).toThrow();
+	expect(() => validator("yo")).toThrow();
+	expect(() => validator(null)).toThrow();
+	expect(() => validator(undefined)).toThrow();
+
+	// valid level
 	expect(validator(myLevel)).toBe(true);
+});
+
+test('only 2 stars timings', () => {
+	expect(() => validator({
+		name: 'tsner',
+		timings: [0],
+	})).toThrow();
+
+	expect(() => validator({
+		name: 'tsner',
+		timings: [1, 4, 5],
+	})).toThrow();
+
+	expect(validator({
+		name: 'tsner',
+		timings: [10, 2],
+	})).toBe(true);
+});
+
+test('2nd star > 3rd star', () => {
+	expect(() => validator({
+		...myLevel,
+		timings: [1, 4],
+	})).toThrow();
+
+	expect(validator({
+		...myLevel,
+		timings: [12345, 12345],
+	})).toBe(true);
+});
+
+test('timings <= 0', () => {
+	expect(() => validator({
+		...myLevel,
+		timings: [-23, 45],
+	})).toThrow();
+
+	expect(() => validator({
+		...myLevel,
+		timings: [23, -45],
+	})).toThrow();
+
+	expect(validator({
+		...myLevel,
+		timings: [0, 0],
+	})).toBe(true);
+});
+
+test('name not empty', () => {
+	expect(() => validator({
+		...myLevel,
+		name: '',
+	})).toThrow();
 });
--- src/validator.ts
+++ src/validator.ts
@@ -1,3 +1,19 @@
+import Ajv from 'ajv';
+
+import schema from './schema.json';
+
+const ajv = new Ajv();
+
+var validate = ajv.compile(schema);
+
 export function validator(level: any) {
+	const valid = validate(level);
+
+	if (!valid) throw validate.errors;
+
+	if (level.timings[0] < level.timings[1]) {
+		throw new Error('Time to get 2rd star must be higher than time to get 3rd star');
+	}
+
 	return true;
 }
