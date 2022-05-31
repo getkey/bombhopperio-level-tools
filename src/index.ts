8212bf11823db896b0cecfd4a3825ebc93d8975f
Julien Mourer <getkey@getkey.eu>
2022-05-31 22:05:52 +0200

    Export svg renderer

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { validateLevel, validatePrefab } from './validator';
 export { polygonIsSimple, polygonArea, canBeDecomposed } from './utils/geom';
 export { getTextBounds, getBounds, centerEntities } from './utils/entities';
 export { Level } from './schemaTypes';
+export { levelToSvg } from './svg';
